d59ec7e7cedcbccb93ac3a9eba47e7e3d7c4b8cc
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-06-28 19:08:35 +0900

    プレイヤーインターフェースを追加

---
+++ rock_paper_scissors/player.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""ジャンケンのプレイヤーインターフェース."""
+from abc import ABCMeta, abstractmethod
+
+from rock_paper_scissors import hand_constant
+
+
+class Player(metaclass=ABCMeta):
+    """ジャンケンのプレイヤーインターフェース."""
+
+    @abstractmethod
+    def start_showdown(self):
+        """手の公開の前に呼ばれる."""
+        pass
+
+    @abstractmethod
+    def showdown(self) -> int:
+        """手の公開."""
+        return hand_constant.HandConstant.ROCK
