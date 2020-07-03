339617dc109ba73e67c9e2326b331c7c27964165
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2020-07-03 10:47:45 +0900

    人間プレイヤーテストを追加

--- tests/test_man_player.py
+++ tests/test_man_player.py
@@ -1,19 +1,77 @@
 #!/usr/bin/env python
-"""人間プレイヤーテスト."""
-# from rock_paper_scissors import man_player
-# from rock_paper_scissors import hand_constant
+"""人間プレイヤーテストクラス."""
+from rock_paper_scissors import man_player
+from rock_paper_scissors import hand_constant
+from rock_paper_scissors import input_keyboard
 
 
-def test_start_showdown_rock():
-    """手の公開(ランダムなので複数回実施してrockが出ることを確認)."""
-    pass
-    # c = cpu_player.CpuPlayer()
-    # try_count = 100
-    # find_rock = False
-    # for _ in range(try_count):
-        # c.start_showdown()
-        # result = c.get()
-        # if result == hand_constant.HandConstant.ROCK:
-            # find_rock = True
-            # break
-    # assert find_rock
+def test_start_showdown_rock(capsys, monkeypatch):
+    """手の公開(rock)."""
+    mp = man_player.ManPlayer()
+    # input関数を置き換えて対応
+    method_name = 'input_string'
+    p = 'rock'
+    monkeypatch.setattr(input_keyboard.InputKeyboard, method_name, lambda x: p)
+    mp.start_showdown()
+    h = mp.get()
+    assert h == hand_constant.HandConstant.ROCK
+
+
+def test_start_showdown_paper(capsys, monkeypatch):
+    """手の公開(paper)."""
+    mp = man_player.ManPlayer()
+    # input関数を置き換えて対応
+    method_name = 'input_string'
+    p = 'paper'
+    monkeypatch.setattr(input_keyboard.InputKeyboard, method_name, lambda x: p)
+    mp.start_showdown()
+    h = mp.get()
+    assert h == hand_constant.HandConstant.PAPER
+
+
+def test_start_showdown_scissors(capsys, monkeypatch):
+    """手の公開(scissors)."""
+    mp = man_player.ManPlayer()
+    # input関数を置き換えて対応
+    method_name = 'input_string'
+    p = 'scissors'
+    monkeypatch.setattr(input_keyboard.InputKeyboard, method_name, lambda x: p)
+    mp.start_showdown()
+    h = mp.get()
+    assert h == hand_constant.HandConstant.SCISSORS
+
+
+def test_get_rock(capsys, monkeypatch):
+    """手の取得(rock)."""
+    mp = man_player.ManPlayer()
+    # input関数を置き換えて対応
+    method_name = 'input_string'
+    p = 'rock'
+    monkeypatch.setattr(input_keyboard.InputKeyboard, method_name, lambda x: p)
+    mp.start_showdown()
+    h = mp.get()
+    assert h == hand_constant.HandConstant.ROCK
+
+
+def test_get_paper(capsys, monkeypatch):
+    """手の取得(paper)."""
+    mp = man_player.ManPlayer()
+    # input関数を置き換えて対応
+    method_name = 'input_string'
+    p = 'paper'
+    monkeypatch.setattr(input_keyboard.InputKeyboard, method_name, lambda x: p)
+    mp.start_showdown()
+    h = mp.get()
+    assert h == hand_constant.HandConstant.PAPER
+
+
+def test_get_scissors(capsys, monkeypatch):
+    """手の取得(scissors)."""
+    mp = man_player.ManPlayer()
+    # input関数を置き換えて対応
+    method_name = 'input_string'
+    p = 'scissors'
+    monkeypatch.setattr(input_keyboard.InputKeyboard, method_name, lambda x: p)
+    mp.start_showdown()
+    h = mp.get()
+    assert h == hand_constant.HandConstant.SCISSORS
